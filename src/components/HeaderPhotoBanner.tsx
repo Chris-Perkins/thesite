04ed6ef46603a5d684dc6844e5710799428adfd4
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2019-11-09 19:18:09 -0800

    Figured out aspect fill for the banner photo :)

--- src/components/HeaderPhotoBanner.tsx
+++ src/components/HeaderPhotoBanner.tsx
@@ -3,14 +3,18 @@ import * as React from "react";
 const pageStyle = {
     width: "100%",
     padding: "0%",
+    paddingTop: "15%",
+    maxHeight: "15%",
+    background: "url('imgs/cat.jpg') no-repeat center center",
+    backgroundSize: "cover",
+    overflow: "invisible",
+    noRepeat: "50% 50%"
 };
 
 export class HeaderPhotoBanner extends React.Component {
     render() {
         return (
-            <div style={pageStyle}>
-                <p>BANNER</p>
-            </div>
+            <div style={pageStyle} />
         );
     }
 }
