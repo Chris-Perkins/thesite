eb548f1a3e1d03417c6b506ef8fd0f4c018f54cc
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2019-11-13 00:10:06 -0800

    Two columns

--- src/components/WeddingInfoBlock.tsx
+++ src/components/WeddingInfoBlock.tsx
@@ -4,19 +4,22 @@ const blockStyle = {
     left: "1%",
     right: "1%",
     top: "1%",
-    bottom: "1%",
+    bottom: "1%"
 };
 
 const infoSection = {
-    padding: "10px"
+    padding: "2%",
+
+    display: "flex",
+    flexWrap: "wrap" as "wrap",
 };
 
-const mapsFrameContainer = {
-    marginTop: "10px",
+const aspectFillPhotoBlock = {
+    backgroundSize: "cover",
+    overflow: "invisible",
+    noRepeat: "50% 50%",
 
-    width: 600, 
-    maxWidth: "100%",
-    height: 400
+    minHeight: 175
 };
 
 const googleMapsFrame = '<iframe top=0 left=0 right=0 bottom=0 width="100%" height="100%" margin=0 position="absolute" frameborder=0 src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJBXoN1P9154gRBFmCgKtrG8o&key=AIzaSyBuYQ1LdimDqupYvIcl9Tm3pA2s5Ye8_yU" allowfullscreen></iframe>';
@@ -27,31 +30,36 @@ export class WeddingInfoBlock extends React.Component {
             <div className="fullContainer" style={{backgroundColor: "#E0E0E0"}}>
                 <div style={blockStyle}>
                     <div style={infoSection}>
-                        <h3>
-                            We're excited to have you come!
-                        </h3>
-                        <p>
-                            The wedding will take place at the Mission Inn located in Howey In The Hills on June 27th, 2020. Complimentary parking will be available at the venue.
-                        </p>
-                        <p style={{marginTop: "1%"}}>
-                            The address and further details about Mission Inn can be viewed at <a href="https://missioninnresort.com/">their website</a> or in the map provided below.
-                        </p>
-                        <div style={mapsFrameContainer} dangerouslySetInnerHTML={{__html: googleMapsFrame}} />
+                        <div className="collapsibleTwoColumnElement">
+                            <h3>
+                                We're excited to have you come!
+                            </h3>
+                            <p>
+                                The wedding will take place at the Mission Inn located in Howey In The Hills on June 27th, 2020. Complimentary parking will be available at the venue.
+                            </p>
+                            <p style={{marginTop: "1%"}}>
+                                The address and further details about Mission Inn can be viewed at <a href="https://missioninnresort.com/">their website</a> or in the map provided.
+                            </p>
+                        </div>
+                        <div className="collapsibleTwoColumnElement" dangerouslySetInnerHTML={{__html: googleMapsFrame}} />
+                    </div>
+                    <div style={infoSection}>
+                        <div className="collapsibleTwoColumnElement">
+                            <h3>
+                                Stay overnight, even before the wedding.
+                            </h3>
+                            <p>
+                                The Mission Inn has graciously offered special rates for guests of our wedding. Rates for guests will be $130 for a room per night, with limited availability. Please contact us to learn how to take advantage of this special rate.
+                            </p>
+                        </div>
+                        <div className="collapsibleTwoColumnElement" style={{...aspectFillPhotoBlock,...{background: "url('./imgs/night.jpg') no-repeat center center"}}} />
                     </div>
                     <div style={infoSection}>
                         <h3>
-                            Stay overnight, even before the wedding.
+                            Dress to your comfort; it's Florida.
                         </h3>
                         <p>
-                            The Mission Inn has graciously offered special rates for guests of our wedding. Rates for guests will be $130 for a room per night, with limited availability. Please contact us to learn how to take advantage of this special rate.
-                        </p>
-                    </div>
-                    <div style={infoSection}>
-                        <h3>
-                            Please attend in whatever you're most comfortable in.
-                        </h3>
-                        <p>
-                            We understand that you may be coming from an area that has different weather than Florida's summer. As such, please dress for your own comfort. However, we do ask that semi-formal standards are followed; leave any gym shorts at home.
+                            Please dress to your comfort in semi-casual clothes; this is a Florida summer. Our only ask is that you leave any gym shorts at home.
                         </p>
                     </div>
                     <div style={infoSection}>
